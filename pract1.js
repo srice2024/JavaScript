fe153e69ed392a22222cb11f7de9a62722e92e54
srice2024 <srice@legacyforcemedia.com>
2024-05-15 18:52:26 -0700

    Initial code creation and sync

---
+++ pract1.js
@@ -0,0 +1,21 @@
+let country = `United States`;
+let continent = `North America`;
+let population = 75;
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+let split = population / 2
+let extra = population + 1
+let more = population > 6
+let ave = population > 33
+let description = `${country} is in ${continent}, and its ${population} million people speak portuguese`
+console.log(split)
+console.log(extra)
+console.log(more)
+console.log(ave)
+console.log(description)
