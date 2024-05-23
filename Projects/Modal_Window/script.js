cb2e76c447d6f54c84a765dbdffa87a66fe68812
srice2024 <srice@legacyforcemedia.com>
2024-05-22 19:45:02 -0700

    Added in notes for clarification about functionality.

--- Projects/Modal_Window/script.js
+++ Projects/Modal_Window/script.js
@@ -7,10 +7,12 @@ const btnCloseModal = document.querySelector('.close-modal');
 //querySelectorAll will allow you to return all the different queries that have the same indetifiers.
 const btnsShowModal = document.querySelectorAll('.show-modal');
 console.log(btnsShowModal);
+//Closeds the modal by adding the hidden class list
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+//Opens the modal by removing the hidden class list
 const openModal = function () {
   //no dots that is only for the selector
   modal.classList.remove('hidden');
