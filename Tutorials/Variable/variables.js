26094b072c086afedcb5599dbcec4ae3bb7d9cf8
srice2024 <srice@legacyforcemedia.com>
2024-05-29 06:43:23 -0700

    Updated for clarity

--- Tutorials/Variable/variables.js
+++ Tutorials/Variable/variables.js
@@ -1,1 +1,30 @@
 "use strict";
+// console.log(me);
+// console.log(job);
+// console.log(birthyear);
+
+// var me = "Shon";
+// //Still in the temporal deadzone but it will not run the birthyear variable because it can not get by the job variabl
+// let job = "accountant";
+// const birthyear = 1995;
+
+// console.log(addDecl(2, 3));
+// // console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+var addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+if (!numProducts) deleteShoppingCart();
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log("All products deleted");
+}
