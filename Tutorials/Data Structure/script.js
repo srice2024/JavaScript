9f17b02bd3dda6551530914b5385f4e07260f9af
srice2024 <srice@legacyforcemedia.com>
2024-06-09 08:13:03 -0700

    Inserted content listing of important elements covered in section

--- Tutorials/Data Structure/script.js
+++ Tutorials/Data Structure/script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+//In this seciton you have the spread, rest, and optional chaining
 // Data needed for a later exercise
 const flights =
   "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
